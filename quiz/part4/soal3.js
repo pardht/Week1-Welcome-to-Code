0a705a8a53f7bbcdead3108bc268a50cec25e4a2
ran <randyparadita31@gmail.com>
2025-04-05 19:25:40 +0800

    soal3 part4 done

---
+++ quiz/part4/soal3.js
@@ -0,0 +1,18 @@
+// Buatlah sebuah fungsi bernama processSentence(), yang akan memproses seluruh parameter yang diinput menjadi 
+// satu kalimat berikut: 
+// "Nama saya [Name], umur saya [Age] tahun, alamat saya di [Address], dan saya punya hobby yaitu [hobby]!"
+
+//bikin fungsinya disini
+
+function processSentence(_name, _age, _address, _hobby) {
+    let sentence = `Nama saya ${_name}, umur saya ${age} tahun, alamat saya di ${_address}, dan hobby saya punya yaitu ${_hobby}`;
+    return sentence;
+}
+
+let name = "Agus";
+let age = 30;
+let address = "Jln. Malioboro, Yogjakarta";
+let hobby = "gaming";
+
+let fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming!"
